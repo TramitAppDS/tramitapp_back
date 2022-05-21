6e61c6b560d07ce70d8f8990fdcb8003dae80d92
Matias Mackenna <matiasmackennad@uc.cl>
2022-05-21 14:10:55 -0400

    feat(login): tramiter and user login

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 const KoaRouter = require('koa-router');
+const jwt = require('koa-jwt');
+
+const { apiSetCurrentUser, apiSetCurrentTramiter } = require('./middlewares/auth');
 
 const hello = require('./routes/hello');
 const index = require('./routes/index');
+const auth = require('./routes/auth');
 
 const router = new KoaRouter();
 
+// unprotected routes
 router.use('/', index.routes());
+router.use('/auth', auth.routes());
+
+// protected routes
+router.use(jwt({ secret: process.env.JWT_SECRET, key: 'authData' }));
+router.use(apiSetCurrentUser);
+router.use(apiSetCurrentTramiter);
+
 router.use('/hello', hello.routes());
 
 module.exports = router;
